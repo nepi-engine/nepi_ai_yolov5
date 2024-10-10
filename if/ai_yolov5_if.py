7f9c86bf140a705ea45d24b0873fd1ba5781a84a
Jason Seawall <jseawall@numurus.com>
2024-10-10 14:26:33 -0700

    ai managerer improvements

--- if/ai_yolov5_if.py
+++ if/ai_yolov5_if.py
@@ -3,7 +3,10 @@
 import sys
 import os
 import os.path
-
+# ROS namespace setup for stand alone testing. Comment out for deployed version
+NEPI_BASE_NAMESPACE = '/nepi/s2x/'
+os.environ["ROS_NAMESPACE"] = NEPI_BASE_NAMESPACE[0:-1] # remove to run as automation script
+import rospy
 import glob
 import subprocess
 import yaml
@@ -13,6 +16,7 @@ import numpy as np
 
 
 from nepi_edge_sdk_base import nepi_ros
+from nepi_edge_sdk_base import nepi_msg
 
 
 from std_msgs.msg import Empty, Float32
@@ -36,21 +40,46 @@ AI_DICT = dict(
     model_prefix = 'yolov5_',
 )
 
+TEST_AI_DICT = {'description': 'Yolov5 ai framework support', 
+'pkg_name': 'nepi_ai_yolov5', 
+'class_name': 'Yolov5AIF', 
+'node_file': 'nepi_ai_yolov5_node.py', 
+'node_name': 'ai_yolov5', 
+'launch_file': 'yolov5_ros.launch', 
+'models_folder': 'yolov5_ros', 
+'model_prefix': 'yolov5_', 
+'if_file': 'ai_yolov5_if.py', 
+'if_path': '/opt/nepi/ros/share/nepi_ai_ifs', 
+'module_name': 'ai_yolov5_if', 
+'active': True
+}
+
+TEST_PUB_SUB_NAMESPACE = "/nepi/s2x/ai_detector_mgr"
+
+TEST_MODELS_LIB_PATH = "/mnt/nepi_storage/ai_models/"
+
+TEST_CLASSIFIER = "common_object_detection_small"
+
+TEST_IMAGE_TOPIC = "color_2d_image"
+
+TEST_THRESHOLD = "0.3"
+
+
 class Yolov5AIF(object):
-    def __init__(self, ai_dict,node_base_namespace,models_lib_path):
-      if node_base_namespace[-1] == "/":
-        node_base_namespace = node_base_namespace[:-1]
-      self.node_base_namespace = node_base_namespace
+    def __init__(self, ai_dict,pub_sub_namespace,models_lib_path, run_test = False):
+      if pub_sub_namespace[-1] == "/":
+        pub_sub_namespace = pub_sub_namespace[:-1]
+      self.pub_sub_namespace = pub_sub_namespace
       self.models_lib_path = models_lib_path
       self.pkg_name = ai_dict['pkg_name']
-      self.node_name = ai_dict['node_name']
+      self.launch_node_name = ai_dict['node_name']
       self.launch_file = ai_dict['launch_file']
       self.model_prefix = ai_dict['model_prefix']
       self.models_folder = ai_dict['models_folder']
       self.models_folder_path =  os.path.join(self.models_lib_path, self.models_folder)
-      rospy.loginfo("Yolov5 models path: " + self.models_folder_path)
-      threshold_namespace = self.node_base_namespace + '/' + self.node_name + '/set_threshold'
-      self.set_threshold_pub = rospy.Publisher(threshold_namespace, Float32, queue_size=1, latch=True)
+      nepi_msg.printMsgInfo("Yolov5 models path: " + self.models_folder_path)
+      if run_test == True:
+        self.startClassifier(TEST_CLASSIFIER, TEST_IMAGE_TOPIC, TEST_THRESHOLD)
     
     #################
     # Yolov5 Model Functions
@@ -62,14 +91,14 @@ class Yolov5AIF(object):
         classifier_classes_list = []
         # Try to obtain the path to Yolov5 models from the system_mgr
         cfg_path_config_folder = os.path.join(self.models_folder_path, 'configs')
-        rospy.loginfo("Yolov5: Looking for models config files in folder: " + cfg_path_config_folder)
+        nepi_msg.printMsgInfo("Yolov5: Looking for models config files in folder: " + cfg_path_config_folder)
         # Grab the list of all existing yolov5 cfg files
         if os.path.exists(cfg_path_config_folder) == False:
-            rospy.loginfo("Yolov5: Failed to find models config files in folder: " + cfg_path_config_folder)
+            nepi_msg.printMsgInfo("Yolov5: Failed to find models config files in folder: " + cfg_path_config_folder)
             return models_dict
         else:
             self.cfg_files = glob.glob(os.path.join(cfg_path_config_folder,'*.yaml'))
-            #rospy.loginfo("Yolov5: Found network config files: " + str(self.cfg_files))
+            #nepi_msg.printMsgInfo("Yolov5: Found network config files: " + str(self.cfg_files))
             # Remove the ros.yaml file -- that one doesn't represent a selectable trained neural net
             for f in self.cfg_files:
                 cfg_dict = dict()
@@ -78,7 +107,7 @@ class Yolov5AIF(object):
                     yaml_stream = open(f, 'r')
                     success = True
                 except Exception as e:
-                    rospy.logwarn("Yolov5: Failed to open yaml file: " + str(e))
+                    nepi_msg.printMsgWarn("Yolov5: Failed to open yaml file: " + str(e))
                 if success:
                     try:
                         # Validate that it is a proper config file and gather weights file size info for load-time estimates
@@ -86,30 +115,30 @@ class Yolov5AIF(object):
                         classifier_keys = list(cfg_dict.keys())
                         classifier_key = classifier_keys[0]
                     except Exception as e:
-                        rospy.logwarn("Yolov5: Failed load yaml data: " + str(e)) 
+                        nepi_msg.printMsgWarn("Yolov5: Failed load yaml data: " + str(e)) 
                         success = False 
                 try: 
                     yaml_stream.close()
                 except Exception as e:
-                    rospy.logwarn("Yolov5: Failed close yaml file: " + str(e))
+                    nepi_msg.printMsgWarn("Yolov5: Failed close yaml file: " + str(e))
                 
                 if success == False:
-                    rospy.logwarn("Yolov5: File does not appear to be a valid A/I model config file: " + f + "... not adding this classifier")
+                    nepi_msg.printMsgWarn("Yolov5: File does not appear to be a valid A/I model config file: " + f + "... not adding this classifier")
                     continue
-                #rospy.logwarn("Yolov5: Import success: " + str(success) + " with cfg_dict " + str(cfg_dict))
+                #nepi_msg.printMsgWarn("Yolov5: Import success: " + str(success) + " with cfg_dict " + str(cfg_dict))
                 cfg_dict_keys = cfg_dict[classifier_key].keys()
                 if ("cfg_file" not in cfg_dict_keys) or ("weight_file" not in cfg_dict_keys):
-                    rospy.logwarn("Yolov5: File does not appear to be a valid A/I model config file: " + f + "... not adding this classifier")
+                    nepi_msg.printMsgWarn("Yolov5: File does not appear to be a valid A/I model config file: " + f + "... not adding this classifier")
                     continue
 
 
                 classifier_name = os.path.splitext(os.path.basename(f))[0]
                 weight_file = os.path.join(self.models_folder_path, "models", "weights",cfg_dict[classifier_key]["weight_file"]["name"])
                 if not os.path.exists(weight_file):
-                    rospy.logwarn("Yolov5: Classifier " + classifier_name + " specifies non-existent weights file " + weight_file + "... not adding this classifier")
+                    nepi_msg.printMsgWarn("Yolov5: Classifier " + classifier_name + " specifies non-existent weights file " + weight_file + "... not adding this classifier")
                     continue
                 classifier_classes_list.append(cfg_dict[classifier_key]['detection_classes']['names'])
-                #rospy.logwarn("Yolov5: Classes: " + str(classifier_classes_list))
+                #nepi_msg.printMsgWarn("Yolov5: Classes: " + str(classifier_classes_list))
                 classifier_name_list.append(classifier_name)
                 classifier_size_list.append(os.path.getsize(weight_file))
             for i,name in enumerate(classifier_name_list):
@@ -119,25 +148,48 @@ class Yolov5AIF(object):
                 model_dict['size'] = classifier_size_list[i]
                 model_dict['classes'] = classifier_classes_list[i]
                 models_dict[model_name] = model_dict
-            #rospy.logwarn("Classifier returning models dict" + str(models_dict))
+            #nepi_msg.printMsgWarn("Classifier returning models dict" + str(models_dict))
         return models_dict
 
 
     def startClassifier(self, classifier, source_img_topic, threshold):
+        source_img_topic = nepi_ros.find_topic(source_img_topic)
+        if source_img_topic == "":
+            nepi_msg.printMsgWarn("Yolov5: Failed to find image topic with str: " + source_img_topic)
+            return
+
+        # Check for files
+        weights_path = os.path.join(self.models_folder_path, "models/weights")
+        if os.path.exists(weights_path) == False:
+            nepi_msg.printMsgWarn("Yolov5: Failed to find weights path: " + weights_path)
+            return
+
+        config_path = os.path.join(self.models_folder_path, "models/cfg")
+        if os.path.exists(config_path) == False:
+            nepi_msg.printMsgWarn("Yolov5: Failed to find configs path: " + config_path)
+            return
+
+        network_param_file_path = os.path.join(self.models_folder_path, "configs", classifier + ".yaml")
+        if os.path.exists(network_param_file_path) == False:
+            nepi_msg.printMsgWarn("Yolov5: Failed to find network params file: " + network_param_file_path)
+            return
+
+
         # Build Yolov5 new classifier launch command
         launch_cmd_line = [
             "roslaunch", self.pkg_name, self.launch_file,
             "pkg_name:=" + self.pkg_name,
-            "node_base_namespace:=" + self.node_base_namespace, 
-            "node_name:=" + self.node_name,
-            "file_name:=" + self.file_name,
-            "weights_path:=" + os.path.join(self.models_folder_path, "models/weights"),
-            "config_path:=" + os.path.join(self.models_folder_path, "models/cfg"),
-            "network_param_file:=" + os.path.join(self.models_folder_path, "configs", classifier + ".yaml"),
+            "model_name:=" + classifier,
+            "pub_sub_namespace:=" + self.pub_sub_namespace, 
+            "node_name:=" + self.launch_node_name,
+            "file_name:=" + self.launch_file,
+            "weights_path:=" + weights_path,
+            "config_path:=" + config_path,
+            "network_param_file:=" + network_param_file_path,
             "source_img_topic:=" + source_img_topic,
             "detector_threshold:=" + str(threshold)
         ]
-        rospy.loginfo("Yolov5: Launching Yolov5 ROS Process: " + str(launch_cmd_line))
+        nepi_msg.printMsgInfo("Yolov5: Launching Yolov5 ROS Process: " + str(launch_cmd_line))
         self.ros_process = subprocess.Popen(launch_cmd_line)
         
 
@@ -147,7 +199,7 @@ class Yolov5AIF(object):
 
 
     def stopClassifier(self):
-        rospy.loginfo("Yolov5: Stopping classifier")
+        nepi_msg.printMsgInfo("Yolov5: Stopping classifier")
         if not (None == self.ros_process):
             self.ros_process.terminate()
             self.ros_process = None
@@ -156,11 +208,12 @@ class Yolov5AIF(object):
         
         #self.current_threshold = 0.3
 
-    def updateClassifierThreshold(self,threshold):
-        self.set_threshold_pub.publish(threshold)
-
-
     
 
 if __name__ == '__main__':
-    Yolov5AIF()
+    node_name = TEST_AI_DICT['node_name']
+    while nepi_ros.check_for_node(node_name):
+        nepi_msg.printMsgInfo("Yolov5: Trying to kill running node: " + node_name)
+        nepi_ros.kill_node(node_name)
+        nepi_ros.sleep(2,10)
+    Yolov5AIF(TEST_AI_DICT,TEST_PUB_SUB_NAMESPACE,TEST_MODELS_LIB_PATH, run_test = True)
--- scripts/nepi_ai_yolov5_node.py
+++ scripts/nepi_ai_yolov5_node.py
@@ -1,174 +1,123 @@
 #!/usr/bin/env python
 
-
+import os
 import rospy
 import torch
 
 from nepi_edge_sdk_base import nepi_ros
 from nepi_edge_sdk_base import nepi_msg
-from nepi_edge_sdk_base import nepi_img
+from nepi_edge_sdk_base import nepi_ais
 
-from sensor_msgs.msg import Image
-
-np.bool = np.bool_
-
+from nepi_edge_sdk_base.ai_node_if import AiNodeIF
 
 # Define your PyTorch model and load the weights
 # model = ...
+
+TEST_DETECTION_DICT_ENTRY = {
+    'class_name': 'chair', # Class String Name
+    'id': 1, # Class Index from Classes List
+    'uid': '', # Reserved for unique tracking by downstream applications
+    'probability': .3, # Probability of detection
+    'box_xmin': 10,
+    'box_ymin': 10,
+    'box_xmax': 100,
+    'box_ymax': 100
+}
+
+
 
 class PytorchDetector():
 
     #######################
     ### Node Initialization
-    DEFAULT_NODE_NAME = "pytorch_ros" # Can be overwitten by luanch command
+    DEFAULT_NODE_NAME = "ai_yolov5" # Can be overwitten by luanch command
     def __init__(self):
         #### APP NODE INIT SETUP ####
         nepi_ros.init_node(name= self.DEFAULT_NODE_NAME)
         self.node_name = nepi_ros.get_node_name()
         self.base_namespace = nepi_ros.get_base_namespace()
+        self.node_namespace = self.base_namespace + self.node_name
         nepi_msg.createMsgPublishers(self)
         nepi_msg.publishMsgInfo(self,"Starting Initialization Processes")
         ##############################
         # Initialize parameters and fields.
-        node_params = nepi_ros.get_param(self,"~")
-        nepi_msg.publishMsgInfo(self,"Starting node params: " + str(node_params))
+        #node_params = nepi_ros.get_param(self,"~")
+        #nepi_msg.publishMsgInfo(self,"Starting node params: " + str(node_params))
+        self.model_name = nepi_ros.get_param(self,"~model_name","")
+        self.pub_sub_namespace = nepi_ros.get_param(self,"~pub_sub_namespace",self.node_namespace)
+        self.weights_path = nepi_ros.get_param(self,"~weights_path","")
+        self.config_path = nepi_ros.get_param(self,"~config_path","")
+        self.source_img_topic = nepi_ros.get_param(self,"~source_img_topic","")
+        threshold_str = nepi_ros.get_param(self,"~detector_threshold","0.5")
         try:
-            self.node_base_namespace = nepi_ros.get_param(self,"~node_base_namespace")
-            self.weights_path = nepi_ros.get_param(self,"~weights_path")
-            self.config_path = nepi_ros.get_param(self,"~config_path")
-            self.source_image_topic = nepi_ros.get_param(self,"~source_img_topic")
-            self.threshold = nepi_ros.get_param(self,"~detector_threshold")
+            self.threshold = float(threshold_str)
         except Exception as e:
-            nepi_msg.publishMsgErr(self,"Failed to get required node info from param server: " + str(e) )
+            self.threshold = 0.5
+            nepi_msg.publishMsgWarn(self,"Failed to convert threshold str " + threshold_str + " to float")
+        if self.model_name == "":
+            nepi_msg.publishMsgWarn(self,"Failed to get required node info from param server: ")
+            rospy.signal_shutdown("Failed to get valid model info from param")
+        else:
+            model_info = nepi_ros.get_param(self,"~ai_model","")
+            if model_info == "":
+                nepi_msg.publishMsgWarn(self,"Failed to get required model info from params: ")
+                rospy.signal_shutdown("Failed to get valid model file paths")
+            else:
+                #nepi_msg.publishMsgWarn(self,"Got model info from param server: " + str(model_info))
+                self.weights_file_path = os.path.join(self.weights_path, model_info['weight_file']['name'])
+                self.config_file_path = os.path.join(self.config_path, model_info['cfg_file']['name'])
+                self.classes = model_info['detection_classes']['names']
 
-        try:
-            model_info = nepi_ros.get_param(self,"~model")
-            self.config_file_path = os.path.join(self.weight_path, model_info.weight_file.name)
-            self.config_file_path = os.path.join(self.config_path, model_info.config_file.name)
-            self.classes = model.detection_clases.names
-        except Exception as e:
-            nepi_msg.publishMsgErr(self,"Failed to get required model info from params: " + str(e) )
-            rospy.signal_shutdown("Failed to get valid model file paths")
-        # Load the model
-        # Initialize the YOLO model.
-        '''
-        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-        self.model = torch.load('/home/nepi/pytorch_ws/models/yolov5', \
-                                    'custom', \
-                                    source="local", \
-                                    path="/home/nepi/pytorch_ws/models/yolov5/checkpoints/yolov5s.pt" \
-                     ).to(self.device)
-        
-        '''
-        # Create AI Node Publishers
-        SOURCE_IMAGE_PUB_TOPIC = self.node_base_namespace + "/source_image"
-        self.source_image_pub = rospy.Publisher(SOURCE_IMAGE_PUB_TOPIC, Image, queue_size = 1)
+                # Load the model
+                # Initialize the YOLO model.
+                '''
+                # Update model settings
+                self.model.conf = 0.3  # Confidence threshold (0-1)
+                self.model.iou = 0.45  # NMS IoU threshold (0-1)
+                self.model.max_det = 20  # Maximum number of detections per image
+                self.model.eval()
 
-        DETECTION_IMAGE_PUB_TOPIC = self.node_base_namespace + "/detection_image"
-        self.detection_image_pub = rospy.Publisher(DETECTION_IMAGE_PUB_TOPIC, Image,  queue_size = 1)
-        
-        FOUND_OBJECT_PUB_TOPIC = self.node_base_namespace + "/found_object"
-        self.found_object_pub = rospy.Publisher(FOUND_OBJECT_PUB_TOPIC, ObjectCount,  queue_size = 1)
+                #  Convert image from ros to cv2
+                cv2_img = nepi_img.rosimg_to_cv2img(source_img_msg)
+                ros_timestamp = img_msg.header.stamp
 
-        BOUNDING_BOXES_PUB_TOPIC = self.node_base_namespace + "/bounding_boxes"
-        self.bounding_boxes_pub = rospy.Publisher(BOUNDING_BOXES_PUB_TOPIC, BoundingBoxes, queue_size = 1)
+                #  Run model against image
+                results = self.model(self.img)
+                '''
 
-        # Create AI Node Subscribers
-        THRSHOLD_SUB_TOPIC = self.node_base_namespace + '/' + self.node_name + '/set_threshold'
-        self.set_threshold_sub = rospy.Subscriber(THRSHOLD_SUB_TOPIC, Float32, self.updateThresholdCb, queue_size=1)
 
-        IMAGE_SUB_TOPIC = self.source_image_topic
-        self.set_threshold_sub = rospy.Subscriber(IMAGE_SUB_TOPIC, Image, self.updateDetectionCb, queue_size=1)
+                '''
+                self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+                self.model = torch.load('/home/nepi/pytorch_ws/models/yolov5', \
+                                            'custom', \
+                                            source="local", \
+                                            path="/home/nepi/pytorch_ws/models/yolov5/checkpoints/yolov5s.pt" \
+                            ).to(self.device)
+                
+                '''
+                self.ai_if = AiNodeIF(node_name = self.node_name, 
+                                    source_img_topic = self.source_img_topic,
+                                    pub_sub_namespace = self.pub_sub_namespace,
+                                    classes_list = self.classes,
+                                    setThresholdFunction = self.setThreshold,
+                                    processDetectionFunction = self.processDetection)
 
-        #########################################################
-        ## Initiation Complete
-        nepi_msg.publishMsgInfo(self,"Initialization Complete")
-        # Spin forever (until object is detected)
-        nepi_ros.spin()
-        #########################################################        
+                #########################################################
+                ## Initiation Complete
+                nepi_msg.publishMsgInfo(self,"Initialization Complete")
+                # Spin forever (until object is detected)
+                nepi_ros.spin()
+                #########################################################        
               
 
-    def updateThresholdCb(self,msg)
-        theshold = msg.data
-        if (threshold < self.MIN_THRESHOLD):
-            threshold = self.MIN_THRESHOLD
-        elif (threshold > self.MAX_THRESHOLD):
-            threshold = self.MAX_THRESHOLD
-        self.updateThreshold(threshold)
+    def setThreshold(self,threshold):
+        pass              
 
-
-    def updateDetectionCb(self,source_img_msg):
-        '''
-        # Update model settings
-        self.model.conf = 0.3  # Confidence threshold (0-1)
-        self.model.iou = 0.45  # NMS IoU threshold (0-1)
-        self.model.max_det = 20  # Maximum number of detections per image
-        self.model.eval()
-
-        #  Convert image from ros to cv2
-        cv2_img = nepi_img.rosimg_to_cv2img(source_img_msg)
-        ros_timestamp = img_msg.header.stamp
-
-        #  Run model against image
-        results = self.model(self.img)
-        '''
-        detection_img_msg = source_img_msg
-        
-
-        self.publishImages(source_img_msg, detection_img_msg)
+    def processDetection(self,cv2_img):
+        return [TEST_DETECTION_DICT_ENTRY]
 
 
 
-    def publishImages(self,ros_source_img, ros_detection_img):
-        ros_timestamp = 
-        if not rospy.is_shutdown():
-            self.source_image_pub.publish(ros_source_img)
-            self.detection_image_pub_image_pub.publish(ros_detection_img)
-
-    def publishDetectionData(self):
-        pass
-        '''
-        detection_dict_list = self.getDetectionData()
-        found_object_msg = ObjectCount()
-        found_object_msg.header.stamp = process_time
-        found_object_msg.count = len(detection_dict_list)
-        if not rospy.is_shutdown():
-            self.found_object_pub.publish(found_object_msg)
-
-        if len(detection_dict_list) > 0:
-            bounding_box_msg_list = []
-            for detection_dict in detection_dict_list:
-                bounding_box_msg = BoundingBox()
-                bounding_box_msg.Class = detection_dict['Class']
-                bounding_box_msg.id = detection_dict['id']
-                bounding_box_msg.uid = detection_dict['uid']
-                bounding_box_msg.probability = detection_dict['probability']
-                bounding_box_msg.xmin = detection_dict['box_xmin']
-                bounding_box_msg.ymin = detection_dict['box_ymin']
-                bounding_box_msg.xmax = detection_dict['box_xmax']
-                bounding_box_msg.ymax = detection_dict['box_ymax']
-                bounding_box_msg_list.append(bounding_box_msg)
-            bounding_boxes_msg = BoundingBoxes()
-            bounding_boxes_msg.header.stamp = process_time
-            bounding_boxes_msg.image_header = image_header
-            bounding_boxes_msg.image_topic = self.source_image_topic
-            bounding_boxes_msg.bounding_boxes = bounding_box_msg_list
-            if not rospy.is_shutdown():
-                self.bounding_boxes_pub.publish(bounding_boxes_msg)
-            '''
-
-
-    def get_classes_color_list(self,classes_str_list):
-        rgb_list = []
-        if len(classes_str_list) > 0:
-            cmap = plt.get_cmap('viridis')
-            color_list = cmap(np.linspace(0, 1, len(classes_str_list))).tolist()
-            for color in color_list:
-                for i in range(3):
-                    rgb.append(int(color[i]*255))
-                rgb_list.append(rgb)
-        return rgb_list
-                
 
 
 if __name__ == '__main__':
